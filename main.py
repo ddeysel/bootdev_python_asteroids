a683d1947c6953e1aeaa79718761dd8747427041
Dieter Deysel <ddeysel@gmail.com>
2025-10-04 09:24:33 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt=0
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000  # Delta time in seconds
+
+    
+if __name__ == "__main__":
+    main()
